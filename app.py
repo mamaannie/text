bb05d1a16c2b5eace0606201a35d72f9fac863e9
mamaannie <99522502+mamaannie@users.noreply.github.com>
2022-03-05 12:13:47 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from textblob import TextBlob
+
+from flask import Flask
+
+app = Flask(__name__)
+
+from flask import render_template,request
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        text = request.form.get("text")
+        print(text)
+        r = TextBlob(text).sentiment
+        return(render_template("index.html", results=r))
+    else:
+        return(render_template("index.html", results="2"))
+
+if __name__=="__main__":
+    app.run()
+
